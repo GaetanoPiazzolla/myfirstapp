52770de936f536cb0eb89c434cdfce92efe4ebb3
GaetanoPiazzolla <gae.piaz@gmail.com>
2017-04-06 16:06:55 +0200

    first commit

---
+++ app/src/main/java/com/gpiazzolla/myfirstapp/MyConstants.java
@@ -0,0 +1,10 @@
+package com.gpiazzolla.myfirstapp;
+
+/**
+ * Created by gpiazzolla on 05/04/2017.
+ */
+public abstract class MyConstants {
+    public static final String HOUR = "time_hour";
+    public static final String MINUTE = "time_minute";
+    public static final String TIME_PICKER = "time_picker";
+}
